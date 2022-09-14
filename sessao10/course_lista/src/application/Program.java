deaf9c6e81064e92a0dcb680c281e92aa0b8762c
franklinperseuDS <fpdll.cid20@uea.edu.br>
2022-09-14 12:55:39 -0400

    Lista - parte 2

--- sessao10/course_lista/src/application/Program.java
+++ sessao10/course_lista/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -13,14 +14,57 @@ public class Program {
         list.add("Maria");
         list.add("Alec");
         list.add("Bob");
-        list.add("anna");
+        list.add("Anna");
+        list.add(2, "Marco");
+
+        System.out.println("Tamanho da lista : "+ list.size());
 
         for (String x: list ) {
             System.out.println(x);
+        }
 
+        System.out.println();
+        System.out.println("Remover Anna");
+        list.remove("Anna");
+        for (String x: list ) {
+            System.out.println(x);
         }
 
 
+        System.out.println();
+        System.out.println("Remover na posição 1");
+        list.remove(1);
+        for (String x: list ) {
+            System.out.println(x);
+        }
 
+        System.out.println();
+        System.out.println("Remover com predicado");
+        list.removeIf(x -> x.charAt(0) == 'M');
+        for (String x: list ) {
+            System.out.println(x);
+        }
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Index of Bob: "+ list.indexOf("Bob"));;
+        System.out.println("Index of Anna: "+ list.indexOf("Anna"));;
+        System.out.println("Caso não exista na lista o index = -1");
+
+
+        list.add("Maria");
+        list.add("Alec");
+        list.add("Anna");
+        list.add(2, "Marco");
+
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        System.out.println("Lista recebendo uma lista filtrada");
+        for (String x: result ) {
+            System.out.println(x);
+        }
+
+        System.out.println("---------------------");
+        System.out.println("Encontrar o primeiro elemento que atenda o predicado");
+        String name = list.stream().filter(x -> x.charAt(0) == 'A').findFirst().orElse(null);
+        System.out.println(name);
     }
 }
