2b90f3e3ff186c78fef89df455542c27d10d5506
franklinperseuDS <fpdll.cid20@uea.edu.br>
2022-09-20 12:35:23 -0400

    adicionando o get set loanLimit

--- sessao14/heranca/src/entities/BusinessAccount.java
+++ sessao14/heranca/src/entities/BusinessAccount.java
@@ -3,12 +3,18 @@ package entities;
 public class BusinessAccount extends Account{
     private double loanLimit;
 
-    public BusinessAccount() {
-        super();
-    }
+    public BusinessAccount() { }
 
     public BusinessAccount(Integer number, String holder, Double balance, double loanLimit) {
         super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(double loanLimit) {
         this.loanLimit = loanLimit;
     }
 
