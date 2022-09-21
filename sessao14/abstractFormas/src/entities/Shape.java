b2ef2ade17f5fae9175498a0c7429b828a423b60
franklinperseuDS <fpdll.cid20@uea.edu.br>
2022-09-21 12:37:54 -0400

    exercicio de Formas(shapes)

--- sessao14/abstractFormas/src/entities/Shape.java
+++ sessao14/abstractFormas/src/entities/Shape.java
@@ -1,2 +1,22 @@
-package entities;public class Sape {
+package entities;
+
+import entities.enums.Color;
+
+public abstract class Shape {
+    private Color color;
+
+    public Shape(){}
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
 }
